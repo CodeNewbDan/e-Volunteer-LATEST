6287fb6ea1a396e5cbbe835d87ef3b0bf1cce1c1
irfanite <maneuverirfan@gmail.com>
2026-06-27 22:49:11 +0800

    OrgDAO and AttendanceDAO

--- src/java/com/model/registration.java
+++ src/java/com/model/registration.java
@@ -3,13 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.model;
+
 /**
  *
- * @author syahmi
- * 18/6/2026 
+ * @author syahmi 18/6/2026
  */
 public class registration {
+
     private int registerId;
+    private int id;
+    private int volunteerId;
+    private int eventId;
     private String registerDate;
     private String attendanceDate;
     private String verificationDate;
@@ -18,12 +22,39 @@ public class registration {
     public registration() {
     }
 
-    public registration(int registerId, String registerDate, String attendanceDate, String verificationDate, String attendanceStatus) {
+    public registration(int registerId, int id, int volunteerId, int eventId, String registerDate, String attendanceDate, String verificationDate, String attendanceStatus) {
         this.registerId = registerId;
+        this.id = id;
+        this.volunteerId = volunteerId;
+        this.eventId = eventId;
         this.registerDate = registerDate;
         this.attendanceDate = attendanceDate;
         this.verificationDate = verificationDate;
         this.attendanceStatus = attendanceStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getVolunteerId() {
+        return volunteerId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVolunteerId(int volunteerId) {
+        this.volunteerId = volunteerId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
     }
 
     public int getRegisterId() {
@@ -65,7 +96,5 @@ public class registration {
     public void setAttendanceStatus(String attendanceStatus) {
         this.attendanceStatus = attendanceStatus;
     }
-    
-    
 
 }
